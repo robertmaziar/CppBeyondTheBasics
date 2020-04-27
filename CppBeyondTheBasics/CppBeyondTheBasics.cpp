cc609df21df12e305b7504b42acc2fda7e6846a3
Robert Maziar <robert.maziar@gmail.com>
2020-04-27 12:51:07 -0400

    Add Standard Library Algorithm Examples
    Added basic examples from the standard library like accumulate, count_if, sort.

--- CppBeyondTheBasics/CppBeyondTheBasics.cpp
+++ CppBeyondTheBasics/CppBeyondTheBasics.cpp
@@ -7,6 +7,9 @@
 #include <chrono>
 #include "Resource.h"
 #include "ListAndVector.h"
+#include <algorithm>
+#include <numeric>
+#include <list>
 
 template <typename Func>
 long long TimeFunc(Func f)
@@ -17,7 +20,6 @@ long long TimeFunc(Func f)
 
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
 }
-
 
 int main()
 {
@@ -45,23 +47,116 @@ int main()
 #pragma endregion
 
 #pragma region Lists
-    int const size = 10'000;
+    //int const size = 10'000;
 
-    std::vector<int> v;
-    v.push_back(0);
+    //std::vector<int> v;
+    //v.push_back(0);
 
-    std::list<int> l;
-    l.push_back(0);
+    //std::list<int> l;
+    //l.push_back(0);
 
-    auto vectormilliseconds = TimeFunc([&]() {BuildVector(size, v); });
-    auto listmilliseconds = TimeFunc([&]() {BuildList(size, l); });
+    //auto vectormilliseconds = TimeFunc([&]() {BuildVector(size, v); });
+    //auto listmilliseconds = TimeFunc([&]() {BuildList(size, l); });
 #pragma endregion
 
 #pragma region Lambdas
-    auto isOdd = [](int candidate) { return candidate % 2 != 0; };
+    //auto isOdd = [](int candidate) { return candidate % 2 != 0; };
 
-    bool is3Odd = isOdd(3);
-    bool is4Odd = isOdd(4);
+    //bool is3Odd = isOdd(3);
+    //bool is4Odd = isOdd(4);
+#pragma endregion
+
+#pragma region Standard Library Algorithms
+    // Populating a vector
+    std::vector<int> v;
+    //for (int i = 0; i < 5; i++)
+    //{
+    //    v.push_back(i);
+    //}
+
+    //for (int i = 0; i < 5;)
+    //{
+    //    v.push_back(i++);
+    //}
+
+    int i = 0;
+    std::generate_n(std::back_inserter(v), 5, [&]() { return i++; });
+
+    // Totalling the elements
+    int total = 0;
+    //for (int index = 0; index < 5; index++)
+    //{
+    //    total += v[index];
+    //}
+
+    //total = 0;
+    //for (int elem : v)
+    //{
+    //    total += elem;
+    //}
+
+    total = std::accumulate(begin(v), end(v), 0);
+
+    // Counting the number of 3's
+    //int count1 = 0;
+    //for (unsigned int i = 0; i < v.size(); i++)
+    //{
+    //    if (v[i] == 3)
+    //    {
+    //        count1++;
+    //    }
+    //}
+
+    //int count2 = 0;
+    //for (auto it = begin(v); it != end(v); it++)
+    //{
+    //    if (*it == 3)
+    //    {
+    //        count2++;
+    //    }
+    //}
+
+    int count3 = std::count(begin(v), end(v), 3);
+
+    // Removing the number 3
+    //auto v2 = v;
+    //for (unsigned int index = 0; index < v2.size(); index++)
+    //{
+    //    if (v2[index] == 3)
+    //    {
+    //        v2.erase(v2.begin() + index);
+    //    }
+    //}
+
+    //auto v3 = v;
+    //for (auto it = begin(v3); it != end(v3); it++)
+    //{
+    //    if (*it == 3)
+    //    {
+    //        v3.erase(it);
+    //    }
+    //}
+
+    auto v4 = v;
+    auto endv4 = std::remove_if(begin(v4), end(v4), [](int elem) { return (elem == 3); });
+    v4.erase(endv4, end(v4));
+
+    // One-liner
+    //v4.erase(std::remove_if(begin(v4), end(v4), [](int elem) { return (elem == 3); }), end(v4));
+
+    v4.push_back(-2);
+    v4.push_back(7);
+    v4.push_back(0);
+    v4.push_back(1);
+
+    std::sort(begin(v4), end(v4));
+
+    bool allpositive = std::all_of(begin(v4), end(v4), [](int elem) { return elem >= 0; });
+
+    std::string s{ "Hello I am a sentence" };
+    auto letter = std::find(begin(s), end(s), 'a');
+
+    auto caps = std::count_if(begin(s), end(s), [](char c) { return (c != ' ') && (toupper(c) == c); });
 #pragma endregion
 
     return 0;
